260c90c1a2442441c9a3304f6a6cd5079450d708
Norah Kim <norahk1049@gmail.com>
2024-10-16 04:59:05 +0000

    message

--- wiki.py
+++ wiki.py
@@ -1,29 +1,69 @@
+from queue import Queue
 import wikipediaapi
 import time
  
+ #setup stuff
 user_agent = "p3_wiki (ki5086no0929@pusd.us)"
 wiki = wikipediaapi.Wikipedia(user_agent, "en")
 
-
-def fetch)links (page):
+# function to return list of links
+def fetch_links (page):
     links_list = []
-    links = page.pinks
+    links = page.links
 
-    for title in links.keys ():
-        links_list.appendg(title)
+    for title in links.keys():
+        links_list.append(title)
 
     return links_list
 
 def wikipedia_game_solver(start_page, target_page):
-    links - fetch_links(start_page
+print("working on it.....")   
+start_time = time.time()
+
+queue = Queue() #queue for which items to check next
+visited = set() # keeps track of visited links
+parent - {} # dictionary to keep track of parent
+
+# start off by adding the start page to our queue and visited
+queue.put(start_page.title)
+visisted.add(Start_page.title)
+
+# keep looping as long as queue is not empty
+while not queue.empty():
+    # get next item in our queue
+    current_page_title = queue.get()
+    # break out of loop if we find page we're looking for
+    if current_page_title == target_page.title:
+        break
+
+    #fetch all the links at the current page
+    current_page = wiki.page(current_page_title)
+    links = fetch_links(current_page)
+
+    #go through each of the links and add them to queue
+    for link in links:
+        # if page hasn't already been visited
+        if link not in visited:
+            queue.put(link)
+            visited.add(link)
+            parent[link] = current_page_title
+            
+    # reconstruct path from target page to start page
+    path = []
+    page_title = target_page.title
+    while page_title != start_page.title:
+        path.append(page_title)
+        page_title = parent[page_title]
+
+    path.append(start_page.title)
+    path.reverse()
+
+    end_time = time.time()
+    print("this algorithm took", end_time - start_time, "seconds.")
+    return path
     
-    # make a loop that checks every item in lnks and prints out a message if 
-    # target_page.title is in that list
-
-
-# start and end pages for our wikipedia game solver
-start_page = wiki.page("Pasadena High School (California)")
-target_page = wiki.page ("Rose Parade")
-
-print (start_page.links)
-git init
+    #start and end pages for our wikipedia game solver
+    start_page = wiki.page("Pasadena High School (California)")
+    target_page = wiki.page("World War II")
+    Path = wikipedia_game_solver(start_page, target_page)
+    print(path)
